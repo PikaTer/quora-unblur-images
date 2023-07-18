d6129a426b621f30ddf9022cb1e522ed7b4a5fca
PikaTer <54795657+PikaTer@users.noreply.github.com>
2023-07-18 19:30:04 +0800

    📃 docs(Project): Version bump to sync with greasefork

--- README.md
+++ README.md
@@ -12,5 +12,3 @@ The script also provides a few shortcuts to helps you in browsing Quora
 ### Shortcut List
 `ctrl` + `alt` + `e`: Expand posts (including images)  
 `ctrl` + `alt` + `b`: Unblur images (in case the script doesn't work automatically)
-
-***
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Quora Unblur Images
 // @namespace    quora-unblur-images
-// @version      0.1
+// @version      0.2
 // @description  Unblur quora images and other utilities
 // @author       PikaTer
 // @match        https://*.quora.com/*
